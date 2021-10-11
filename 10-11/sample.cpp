cd6104198a808e861a03316a00e06ad1f55e2499
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2021-10-11 10:21:20 +0900

    add:sample.cpp

---
+++ 10-11/sample.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+class Vector2{
+    private:
+        int x;
+        int y;
+    public:
+        Vector2(int x = 0, int y = 0){
+            this->x = x;
+            this->y = y;
+        }
+        void show(){
+            cout << "(" << x << "," << y << ")" << endl;
+        }
+        int operator*(const Vector2& v){
+            return (x*v.x + y*v.y);
+        }
+        friend Vector2 operator*(const int& num,const Vector2& v);
+        Vector2 operator+(const Vector2& v){
+            return Vector2(x + v.x, y + v.y);
+        }
+        Vector2 operator++(){
+            return Vector2(++x, ++y);
+        }
+        Vector2 operator++(int d){
+            return Vector2(x++, y++);
+        }
+        Vector2 operator+(){
+            return *this;
+        }
+        Vector2 operator-(){
+            return Vector2(-x, -y);
+        }
+};
+Vector2 operator*(const int& num,const Vector2& v){
+    return (num*v.x, num*v.y);
+}
+
+int main(){
+    Vector2 v1(1,3);
+    Vector2 v2(5,2);
+    Vector2 v;
+
+    ++v1;
+    v = 2 * v1 + (-v1) * (+v2 * v1);
+    v++;
+    v.show();
+
+    return 0;
+}
