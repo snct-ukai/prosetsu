06504f837a43742db269b4c88d47f00b6216a7b8
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2021-10-18 10:12:52 +0900

    add:say_function

---
+++ 10-18/say_dyn.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+class Animal{
+    public:
+        virtual void say() = 0;
+};
+
+class Human : public Animal{
+    public:
+        void say(){}
+        void speak(){
+            cout << "speak" << endl;
+        }
+};
+
+class Dog : public Animal{
+    public:
+        void say(){}
+        void bark(){
+            cout << "bark" << endl;
+        }
+};
+
+void say(Animal* a){
+    Human* human = dynamic_cast<Human*>(a);
+    Dog* dog = dynamic_cast<Dog*>(a);
+    if(human){
+        human -> speak();
+    }
+    if(dog){
+        dog -> bark();
+    }
+}
+
+int main(void){
+    Human human;
+    Dog dog;
+
+    say(&human);
+    say(&dog);
+}
---
+++ 10-18/say_poli.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class Animal{
+    public:
+        virtual void say() = 0;
+};
+
+class Human : public Animal{
+    public:
+        void say(){speak();}
+        void speak(){
+            cout << "speak" << endl;
+        }
+};
+
+class Dog : public Animal{
+    public:
+        void say(){bark();}
+        void bark(){
+            cout << "bark" << endl;
+        }
+};
+
+void say(Animal* a){
+    a -> say();
+}
+
+int main(void){
+    Human human;
+    Dog dog;
+
+    say(&human);
+    say(&dog);
+}
