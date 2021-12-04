dcb3f22a49bb52e100b488e94dd8444640716509
snct-ukai <73654679+snct-ukai@users.noreply.github.com>
2021-12-04 23:28:25 +0900

    update:sample1-2.cpp

--- 11-29/sample1-2.cpp
+++ 11-29/sample1-2.cpp
@@ -6,12 +6,12 @@ int main(void){
     int month, day;
     cout << "今日は何月？";
     cin >> month;
+    cout << "今日は何日";
+    cin >> day;
     try{
         if(month < 1 || month > 12){
             throw out_of_range("月の入力値がおかしい\n");
         }
-        cout << "今日は何日";
-        cin >> day;
         if(day < 1 || day > 31){
             throw out_of_range("日の入力値がおかしい\n");
         }
