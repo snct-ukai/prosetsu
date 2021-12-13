22ffcf46467b4164bcf758e071ddbaeb559d4888
snct-ukai <73654679+snct-ukai@users.noreply.github.com>
2021-12-13 12:44:56 +0900

    update

--- 12-6/sample4.cpp
+++ 12-6/sample4.cpp
@@ -6,12 +6,10 @@ int main(void){
     ifstream in("data.txt");
 
     double sum = 0;
-    while(true){
-        double d;
-        try{
-            in >> d;
-        }catch(exception e){
-            break;
-        }
+    double d;
+    while(in.eof()){
+        in >> d;
+        sum += d;
     }
+    cout << sum << endl;
 }
