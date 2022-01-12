2525ef8fef0b544f761f9543da624b493213a864
snct-ukai <73654679+snct-ukai@users.noreply.github.com>
2022-01-12 13:29:28 +0900

    update:file import

--- 12-20/sample1.cpp
+++ 12-20/sample1.cpp
@@ -4,12 +4,17 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-    if(argc != 2){
+    if(argc != 3){
         cout << "ファイルを二つ指定してください。" << endl;
-        return;
+        return -1;
     }
-    ifstream input(argv[0]);
-    ofstream output(argv[1]);
+    ifstream input(argv[1]);
+    ofstream output(argv[2]);
+
+    if(input.fail() & output.fail()){
+        cerr << "ファイルが正常に読み込めませんでした" << endl;
+        return -1;
+    }
 
     while(!input.eof()){
         string str;
